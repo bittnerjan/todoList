7b9a87329c801452227a516dfbb3fabb9fd6a1e2
Jan Bittner <j.bittner815@gmail.com>
2021-04-07 11:23:01 +0200

    add delete logic

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const input = document.getElementById("input");
 const button = document.getElementById("add-button");
 const list = document.getElementById("list");
+let listItems = document.getElementsByTagName("li");
 
 button.addEventListener("click", addTask);
+
+appendRemoveEl();
+
+function appendRemoveEl() {
+  for (let i = 0; i < listItems.length; i++) {
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    listItems[i].appendChild(span);
+  }
+}
 
 function addTask() {
   if (input.value === "") {
@@ -12,5 +25,15 @@ function addTask() {
     let itemText = document.createTextNode(input.value);
     listItem.appendChild(itemText);
     list.appendChild(listItem);
+    appendRemoveEl();
   }
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
 }
